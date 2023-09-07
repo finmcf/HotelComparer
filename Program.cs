562d9381ab693146b7bdef574872f7f10e7605df
finmcf <92210179+finmcf@users.noreply.github.com>
2023-09-07 09:25:47 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddMemoryCache(); // Existing line
+builder.Services.AddMemoryCache(); 
 
 // Existing service registrations
 builder.Services.AddScoped<IAmadeusApiService, AmadeusApiService>();
---
+++ Services/HotelDataServiceEdited.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HotelComparer.Services
+{
+	public class HotelDataServiceEdited
+	{
+		public HotelDataServiceEdited()
+		{
+		}
+	}
+}
+
