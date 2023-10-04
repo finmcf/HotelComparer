64dba3d55bc17ae3c4760f3e23bce672c70a8551
finmcf <92210179+finmcf@users.noreply.github.com>
2023-10-04 15:36:59 +0100

    updates

---
+++ Examples/HotelOffersExample.cs
@@ -0,0 +1,16 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+
+namespace YourApiNamespace.Examples
+{
+    public class HotelOffersExample : IExamplesProvider<HotelOffersResponse>
+    {
+        public HotelOffersResponse GetExamples()
+        {
+            return new HotelOffersResponse // Replace with actual DTO
+            {
+                // Populate the fields with example data
+                // ...
+            };
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using System;
+using HotelComparer.Models;
 using HotelComparer.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models; // Add this namespace
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using YourApiNamespace.Examples;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,30 +23,26 @@ builder.Services.AddScoped<IHotelDataService, HotelDataService>();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel Comparer API", Version = "v1" });
+    c.ExampleFilters();
 });
+
+// Register Swagger examples
+builder.Services.AddSwaggerExamplesFromAssemblyOf<HotelOffersExample>(); // Updated this line
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-
-    // Enable middleware to serve generated Swagger as a JSON endpoint
     app.UseSwagger();
-
-    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-    // specifying the Swagger JSON endpoint
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hotel Comparer API v1");
-        c.RoutePrefix = string.Empty; // Make Swagger UI the app's home page in development
+        c.RoutePrefix = string.Empty;
     });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
