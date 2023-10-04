3fea1a7c67325335ad7fce52489c02bddf2b8032
finmcf <92210179+finmcf@users.noreply.github.com>
2023-10-04 04:35:05 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -2,24 +2,40 @@ using HotelComparer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models; // Add this namespace
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Register services
 builder.Services.AddControllers();
-builder.Services.AddMemoryCache(); 
+builder.Services.AddMemoryCache();
 
 builder.Services.AddScoped<IAmadeusApiService, AmadeusApiService>();
 builder.Services.AddScoped<IAmadeusApiTokenService, AmadeusApiTokenService>();
-
-
 builder.Services.AddScoped<IHotelDataService, HotelDataService>();
+
+// Register the Swagger generator, defining one or more Swagger documents
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel Comparer API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+
+    // Enable middleware to serve generated Swagger as a JSON endpoint
+    app.UseSwagger();
+
+    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+    // specifying the Swagger JSON endpoint
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hotel Comparer API v1");
+        c.RoutePrefix = string.Empty; // Make Swagger UI the app's home page in development
+    });
 }
 
 app.UseHttpsRedirection();
