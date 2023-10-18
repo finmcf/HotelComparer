316bdc4d1d3f2854b6cf65b2b92a42e872f27f43
finmcf <92210179+finmcf@users.noreply.github.com>
2023-10-18 06:02:03 +0100

    updates

--- Middleware/ApiKeyMiddleware.cs
+++ Middleware/ApiKeyMiddleware.cs
@@ -1,22 +1,26 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using HotelComparer.Data;
 using HotelComparer.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace HotelComparer.Middleware
 {
     public class ApiKeyMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly IMemoryCache _cache;
 
-        public ApiKeyMiddleware(RequestDelegate next)
+        public ApiKeyMiddleware(RequestDelegate next, IMemoryCache cache)
         {
             _next = next;
+            _cache = cache;
         }
 
-        public async Task InvokeAsync(HttpContext context)
+        public async Task InvokeAsync(HttpContext context, ApiDbContext dbContext)
         {
             var clientId = context.Request.Headers["ClientId"].FirstOrDefault();
             var clientSecret = context.Request.Headers["ClientSecret"].FirstOrDefault();
@@ -27,18 +31,30 @@ namespace HotelComparer.Middleware
                 return;
             }
 
-            // Get the scoped ApiDbContext from the service provider
-            var dbContext = (ApiDbContext)context.RequestServices.GetService(typeof(ApiDbContext));
-
-            var apiKey = await dbContext.ApiKeys
-                .Include(k => k.Permissions)
-                .FirstOrDefaultAsync(k => k.ClientId == clientId && k.ClientSecret == clientSecret);
-
-            if (apiKey == null)
+            var cacheKey = $"{clientId}-{clientSecret}";
+            if (!_cache.TryGetValue(cacheKey, out ApiKey apiKey))
             {
-                context.Response.StatusCode = 401; // Unauthorized
-                return;
+                apiKey = await dbContext.ApiKeys
+                    .Include(k => k.Permissions)
+                    .FirstOrDefaultAsync(k => k.ClientId == clientId && k.ClientSecret == clientSecret);
+
+                if (apiKey == null)
+                {
+                    context.Response.StatusCode = 401; // Unauthorized
+                    return;
+                }
+
+                // Customize the cache options as needed
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    // Keep in cache for this time, reset time if accessed.
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(2))
+                    // Keep in cache for this time, regardless of access.
+                    .SetAbsoluteExpiration(TimeSpan.FromHours(1));
+
+                _cache.Set(cacheKey, apiKey, cacheEntryOptions);
             }
+
+            // Add additional logic as needed
 
             await _next(context);
         }
